ecec47ba6b9a7b7187a15693954ccbfdaccceace
Richard Absin <richard.absin24@outlook.com>
2019-02-11 13:05:53 -0800

    Twitter Part 2 (FINAL)

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -35,10 +35,16 @@ class TweetCell: UITableViewCell {
         }
     }
     @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error in retweeting: \(error)")
+        })
     }
     
     var favorited:Bool = false
     var tweetId:Int = -1
+    var retweeted:Bool = false
     
     
     func setFavorited(_ isFavorited:Bool) {
@@ -50,6 +56,18 @@ class TweetCell: UITableViewCell {
            favButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
         }
     }
+    
+    func setRetweeted(_ isRetweeted:Bool) {
+        if (isRetweeted) {
+            retweetButton.setImage(UIImage(named:"retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }
+        else {
+            retweetButton.setImage(UIImage(named:"retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
